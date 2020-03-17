1eaa7968bcf222fe54824ef26f4c928175168a53
Prashanth Pai <prashanth.pai@plivo.com>
2020-03-17 22:07:18 +0530

    Fix panic when table does not exist
    Signed-off-by: Prashanth Pai <prashanth.pai@plivo.com>

--- book.go
+++ book.go
@@ -30,29 +30,29 @@ func allBooks() ([]Book, error) {
 	books := []Book{}
 
 	rows, err := db.Query(`SELECT id, name, author, pages, publication_date FROM books order by id`)
+	if err != nil {
+		return nil, err
+	}
 	defer rows.Close()
-	if err == nil {
-		for rows.Next() {
-			var id int
-			var name string
-			var author string
-			var pages int
-			var publicationDate pq.NullTime
 
-			err = rows.Scan(&id, &name, &author, &pages, &publicationDate)
-			if err == nil {
-				currentBook := Book{ID: id, Name: name, Author: author, Pages: pages}
-				if publicationDate.Valid {
-					currentBook.PublicationDate = publicationDate.Time
-				}
+	for rows.Next() {
+		var id int
+		var name string
+		var author string
+		var pages int
+		var publicationDate pq.NullTime
 
-				books = append(books, currentBook)
-			} else {
-				return books, err
-			}
+		err = rows.Scan(&id, &name, &author, &pages, &publicationDate)
+		if err != nil {
+			return books, err
 		}
-	} else {
-		return books, err
+
+		currentBook := Book{ID: id, Name: name, Author: author, Pages: pages}
+		if publicationDate.Valid {
+			currentBook.PublicationDate = publicationDate.Time
+		}
+
+		books = append(books, currentBook)
 	}
 
 	return books, err
